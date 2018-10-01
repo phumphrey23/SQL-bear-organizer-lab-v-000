5981998f4bf90a1cfcf526fd459bbabe00232c48
Precious Humphrey <phumphrey23@gmail.com>
2018-10-01 12:36:21 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 4, "male", "brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 6, "female", "grey", "happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 3, "female", "black", "scared", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 5, "male", "black", "goofy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 8, "male", "black", "scared", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 2, "female", "brown", "sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 7, "male", "purple", "happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (NULL, 8, "female", "black", "goofy", 1);
